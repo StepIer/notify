297e9af53a3446490ca69cce48cd6d9ac4c10389
Stepan Kondratiev <Stepan.Kondratiev@softeq.com>
2022-05-04 16:37:16 +0300

    events logic add

--- app/src/main/java/com/example/notify/data/events/db/EventsDatabase.kt
+++ app/src/main/java/com/example/notify/data/events/db/EventsDatabase.kt
@@ -4,10 +4,10 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.notify.data.LocalDateTimeConverters
-import com.example.notify.data.notes.model.NoteEntity
+import com.example.notify.data.events.model.EventEntity
 
-@Database(entities = [NoteEntity::class], version = 1)
-@TypeConverters(LocalDateTimeConverters::class, LocalDateTimeConverters::class)
+@Database(entities = [EventEntity::class], version = 1)
+@TypeConverters(LocalDateTimeConverters::class)
 abstract class EventsDatabase : RoomDatabase() {
     abstract fun eventsDao(): EventsDao
 }
